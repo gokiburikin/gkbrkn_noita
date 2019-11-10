4dca9dc527730669143f956366857a2ec6251508
gokiburikin <gokiburikin@gmail.com>
2019-11-10 08:23:57 -0500

    Fix Quick Swap swapping on items that had an alt fire function

--- files/gkbrkn/world_post_update.lua
+++ files/gkbrkn/world_post_update.lua
@@ -53,32 +53,36 @@ for _,player in pairs( players ) do
     end
 
     local controls = EntityGetFirstComponent( player, "ControlsComponent" );
-    if controls ~= nil then
-        local alt_fire = ComponentGetValue( controls, "mButtonDownFire2" );
-        local alt_fire_frame = ComponentGetValue( controls, "mButtonFrameFire2" );
-        if alt_fire == "1" and GameGetFrameNum() == tonumber(alt_fire_frame) then
-            if quick_swapped == false then
-                quick_swapped = true;
-                local inventory = nil;
-                local swap_inventory = nil;
-                for _,child in pairs(EntityGetAllChildren( player )) do
-                    if EntityGetName(child) == "inventory_quick" then
-                        inventory = child;
-                    elseif EntityGetName(child) == "gkbrkn_swap_inventory" then
-                        swap_inventory = child;
+    local inventory2 = EntityGetFirstComponent( player, "Inventory2Component" );
+    if controls ~= nil and inventory2 ~= nil then
+        local active_item = ComponentGetValue( inventory2, "mActiveItem" );
+        if active_item == nil or EntityHasTag( active_item, "wand" ) == true then
+            local alt_fire = ComponentGetValue( controls, "mButtonDownFire2" );
+            local alt_fire_frame = ComponentGetValue( controls, "mButtonFrameFire2" );
+            if alt_fire == "1" and GameGetFrameNum() == tonumber(alt_fire_frame) then
+                if quick_swapped == false then
+                    quick_swapped = true;
+                    local inventory = nil;
+                    local swap_inventory = nil;
+                    for _,child in pairs(EntityGetAllChildren( player )) do
+                        if EntityGetName(child) == "inventory_quick" then
+                            inventory = child;
+                        elseif EntityGetName(child) == "gkbrkn_swap_inventory" then
+                            swap_inventory = child;
+                        end
+                    end
+                    if inventory ~= nil and swap_inventory ~= nil then
+                        local inventory_entities = EntityGetAllChildren( inventory ) or {};
+                        local swap_inventory_entities = EntityGetAllChildren( swap_inventory ) or {};
+                        for _,child in pairs(inventory_entities) do EntityRemoveFromParent( child ); end
+                        for _,child in pairs(swap_inventory_entities) do EntityRemoveFromParent( child ); end
+                        for _,child in pairs(inventory_entities) do EntityAddChild( swap_inventory, child ); end
+                        for _,child in pairs(swap_inventory_entities) do EntityAddChild( inventory, child ); end
                     end
                 end
-                if inventory ~= nil and swap_inventory ~= nil then
-                    local inventory_entities = EntityGetAllChildren( inventory ) or {};
-                    local swap_inventory_entities = EntityGetAllChildren( swap_inventory ) or {};
-                    for _,child in pairs(inventory_entities) do EntityRemoveFromParent( child ); end
-                    for _,child in pairs(swap_inventory_entities) do EntityRemoveFromParent( child ); end
-                    for _,child in pairs(inventory_entities) do EntityAddChild( swap_inventory, child ); end
-                    for _,child in pairs(swap_inventory_entities) do EntityAddChild( inventory, child ); end
-                end
+            else
+                quick_swapped = false;
             end
-        else
-            quick_swapped = false;
         end
     end
 end
