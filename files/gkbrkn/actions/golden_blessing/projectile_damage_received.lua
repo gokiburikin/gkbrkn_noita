1d01d025c9498ca1888ab3f4b95ac27eb7a925af
gokiburikin <gokiburikin@gmail.com>
2019-11-11 21:06:58 -0500

    Fix the Golden Blood fix

--- files/gkbrkn/actions/golden_blessing/projectile_damage_received.lua
+++ files/gkbrkn/actions/golden_blessing/projectile_damage_received.lua
@@ -1,6 +1,6 @@
 function damage_received( damage, message, entity_thats_responsible, is_fatal  )
     local entity = GetUpdatedEntityID();
-    if EntityHasTag( entity, "boss_centipede" ) === false then
+    if EntityHasTag( entity, "boss_centipede" ) == false then
         local damage_models = EntityGetComponent( entity, "DamageModelComponent" );
         if is_fatal then
             EntityConvertToMaterial( entity, "gold" );
